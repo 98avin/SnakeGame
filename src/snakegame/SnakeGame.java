71c8b8198fa49f2131a9dc58cddbfcb6bab101cb
cs5 <cs5@cs5-PC>
2015-06-23 09:40:35 -0400

    hey

--- src/snakegame/SnakeGame.java
+++ src/snakegame/SnakeGame.java
@@ -15,7 +15,7 @@ import javax.swing.SwingConstants;
  *
  * @author cs1
  */
-///CHECKING IF PUSHES WORK OWEN SKINNER
+///CHECKING IF PUSHES ERIC
 public class SnakeGame {
 
     public static void main(String[] args) {
