f39d841385bc45002965aac70ee443ec41ebb499
kangr6 <kangr6@al3.rowanads.rowan.edu>
2015-06-24 12:26:21 -0400

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/snakegame/SnakePanel.java
+++ src/snakegame/SnakePanel.java
@@ -101,20 +101,24 @@ public class SnakePanel extends JPanel {
         int e = (int) (Math.random() * 1000 % 15);
         insane = new Color(q, w, e);
         for (int col = 0; col < NumCols; col++) {
-            if (col < bernie.getX() + 30 && col > bernie.getX() - 30||col < bernita.getX() + 30 && col > bernita.getX() - 30) {
+            if (col < bernie.getX() + 30 ||col < bernita.getX() + 30) {
+                if((col > bernie.getX() - 30 || col > bernita.getX() - 30)){
                 for (int row = 0; row < NumRows; row++) {
-                    if ((row < bernie.getY() + 25 && row > bernie.getY() - 25)|| (row < bernita.getY() + 25 && row > bernita.getY() - 25)) {
+                    if ((row < bernie.getY() + 25 || row < bernita.getY() + 25)) {
+                        if(( row > bernie.getY() - 25|| row > bernita.getY() - 25)){
                         g.setColor(colors[readSquare(col, row)]);
                         g.fillRect(col * squareSize, row * squareSize, squareSize, squareSize);
                         g.setColor(Color.gray);
                         ///g.drawRect(col * squareSize, row * squareSize, squareSize, squareSize);
 
                     }
+                    }
             //g2d.drawImage(null, 0, 0, null);
                     //g2d.drawImage(null, bernie.getX(), bernie.getY(), null);
 
                     //g.translate(-1, -1);
                 }
+        }
             }
         }
 }
